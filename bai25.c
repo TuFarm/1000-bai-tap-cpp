d25b6eb82245d95d9a3146cb3d6f78d8da28eeea
TuFarm <kpnkt.vn@gmail.com>
2024-10-04 22:32:26 +0700

    24-25

---
+++ bai25.c
@@ -0,0 +1,23 @@
+/*Bai 25 - Liet ke tat ca cac uoc so "chan" cua so nguyen duong n*/
+
+#include <stdio.h>
+int main() {
+    int n;
+    do {
+        printf("Enter a number: ");
+        scanf_s("%d", &n);
+        if (n <= 0) {
+            printf("Invalid Input, Try again!\n");
+        }
+    } while (n <= 0);
+
+    int i = 1;
+    for (i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            if (i % 2 == 0) {
+                printf("%d ", i);
+            }
+        }
+    }
+    return 0;
+}
